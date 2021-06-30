b9ac583b1296bdf4e064bedcf53640ac14154ffa
Ida Friisberg <idfri@dfds.com>
2021-06-30 13:13:37 +0200

    add model_selection.py

---
+++ ds-toolbox/model_selection/model_selection.py
@@ -0,0 +1,35 @@
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error, roc_curve, auc, accuracy_score
+from sklearn.base import BaseEstimator, ClassifierMixin
+
+
+class ClfSwitcher(BaseEstimator, ClassifierMixin):
+
+    def __init__(
+        self, 
+        estimator = RandomForestClassifier(),
+    ):
+        """
+        A Custom BaseEstimator that can switch between classifiers.
+        :param estimator: sklearn object - The classifier
+        """ 
+
+        self.estimator = estimator
+
+
+    def fit(self, X, y=None, **kwargs):
+        self.estimator.fit(X, y)
+        return self
+
+
+    def predict(self, X, y=None):
+        return self.estimator.predict(X)
+
+
+    def predict_proba(self, X):
+        return self.estimator.predict_proba(X)
+
+
+    def score(self, X, y):
+        return self.estimator.score(X, y)
