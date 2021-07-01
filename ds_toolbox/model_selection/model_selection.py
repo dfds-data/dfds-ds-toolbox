80cc6c00f2e9531c249a3b172da140d0042e3672
Ida Friisberg <idfri@dfds.com>
2021-07-01 14:16:26 +0200

    black formatting

--- ds_toolbox/model_selection/model_selection.py
+++ ds_toolbox/model_selection/model_selection.py
@@ -3,58 +3,49 @@ from sklearn.base import BaseEstimator, ClassifierMixin, RegressorMixin
 
 
 class ClfSwitcher(BaseEstimator, ClassifierMixin):
-
     def __init__(
-        self, 
-        estimator = RandomForestClassifier(),
+        self,
+        estimator=RandomForestClassifier(),
     ):
         """
         A Custom BaseEstimator that can switch between classifiers.
         :param estimator: sklearn object - The classifier
-        """ 
+        """
 
         self.estimator = estimator
-
 
     def fit(self, X, y=None, **kwargs):
         self.estimator.fit(X, y)
         return self
 
-
     def predict(self, X, y=None):
         return self.estimator.predict(X)
 
-
     def predict_proba(self, X):
         return self.estimator.predict_proba(X)
-
 
     def score(self, X, y):
         return self.estimator.score(X, y)
 
 
 class RegSwitcher(BaseEstimator, RegressorMixin):
-
     def __init__(
-        self, 
-        estimator = RandomForestRegressor(),
+        self,
+        estimator=RandomForestRegressor(),
     ):
         """
         A Custom BaseEstimator that can switch between regressors.
         :param estimator: sklearn object - The regressor
-        """ 
+        """
 
         self.estimator = estimator
-
 
     def fit(self, X, y=None, **kwargs):
         self.estimator.fit(X, y)
         return self
 
-
     def predict(self, X, y=None):
         return self.estimator.predict(X)
-
 
     def score(self, X, y):
         return self.estimator.score(X, y)
