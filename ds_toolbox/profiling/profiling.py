0a58298d7b6bd6ad33e28dbceb404c1517654025
Pablo <pacaa@dfds.com>
2021-07-27 11:57:56 +0200

    black formatting

--- ds_toolbox/profiling/profiling.py
+++ ds_toolbox/profiling/profiling.py
@@ -2,8 +2,9 @@ import cProfile
 from pathlib import Path
 from functools import wraps
 
+
 def profileit(profiles_path: Path, profile_name: str):
-    """Used as decorator. Generate stats file from the execution time profile of a function 
+    """Used as decorator. Generate stats file from the execution time profile of a function
     using cProfile. The stats files are saved as <profiles_path>/<profile_name>.stats
     See https://docs.python.org/3/library/profile.html#module-cProfile
 
@@ -15,9 +16,13 @@ def profileit(profiles_path: Path, profile_name: str):
         @wraps(func)
         def wrapper(*args, **kwargs):
             if not isinstance(profiles_path, Path):
-                raise TypeError(f"profiles_path must be an instance of <class pathlib.Path>, got {type(profiles_path)}")
+                raise TypeError(
+                    f"profiles_path must be an instance of <class pathlib.Path>, got {type(profiles_path)}"
+                )
             if not isinstance(profile_name, str):
-                raise TypeError(f"profile_name must be an instance of <class str>, got {type(profile_name)}")
+                raise TypeError(
+                    f"profile_name must be an instance of <class str>, got {type(profile_name)}"
+                )
             prof = cProfile.Profile()
             retval = prof.runcall(func, *args, **kwargs)
             profiles_path.mkdir(exist_ok=True, parents=True)
--- ds_toolbox/profiling/tests/test_profiling.py
+++ ds_toolbox/profiling/tests/test_profiling.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import time
 import pytest
 
+
 def test_profiling_successful(tmp_path):
     @profileit(profiles_path=tmp_path, profile_name="test")
     def dummy_function():
@@ -10,20 +11,22 @@ def test_profiling_successful(tmp_path):
 
     dummy_function()
 
+
 def test_profiling_invalid_path():
     with pytest.raises(TypeError):
+
         @profileit(profiles_path="invalid", profile_name="test")
         def dummy_function():
             time.sleep(0.1)
 
         dummy_function()
 
+
 def test_profiling_invalid_profile_name(tmp_path):
     with pytest.raises(TypeError):
+
         @profileit(profiles_path=tmp_path, profile_name=2)
         def dummy_function():
             time.sleep(0.1)
 
         dummy_function()
-    
-
