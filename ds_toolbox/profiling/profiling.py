3f4a7973853e3a2d5011140f2a8c60bcfede1465
Kasper Primdal Lauritzen <klaur@dfds.com>
2021-10-26 14:28:09 +0200

    Rename profileit arguments.
    It is in the profiling module. We can infer "profile" from context.

--- ds_toolbox/profiling/profiling.py
+++ ds_toolbox/profiling/profiling.py
@@ -3,33 +3,31 @@ from functools import wraps
 from pathlib import Path
 
 
-def profileit(profiles_path: Path, profile_name: str):
+def profileit(path: Path, name: str):
     """Generate stats file from the execution time profile of a function.
 
     Used as decorator.
-    Generate profiling stats using cProfile. The stats files are saved as <profiles_path>/<profile_name>.stats
+    Generate profiling stats using cProfile. The stats files are saved as <path>/<name>.stats
     See https://docs.python.org/3/library/profile.html#module-cProfile
 
     Args:
-        profiles_path: folder path where the stats file will be saved
-        profile_name: name of the stats file
+        path: folder path where the stats file will be saved
+        name: name of the stats file
     """
 
     def inner(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            if not isinstance(profiles_path, Path):
+            if not isinstance(path, Path):
                 raise TypeError(
-                    f"profiles_path must be an instance of <class pathlib.Path>, got {type(profiles_path)}"
+                    f"path must be an instance of <class pathlib.Path>, got {type(path)}"
                 )
-            if not isinstance(profile_name, str):
-                raise TypeError(
-                    f"profile_name must be an instance of <class str>, got {type(profile_name)}"
-                )
+            if not isinstance(name, str):
+                raise TypeError(f"name must be an instance of <class str>, got {type(name)}")
             prof = cProfile.Profile()
             retval = prof.runcall(func, *args, **kwargs)
-            profiles_path.mkdir(exist_ok=True, parents=True)
-            prof.dump_stats(profiles_path / f"{profile_name}.stats")
+            path.mkdir(exist_ok=True, parents=True)
+            prof.dump_stats(path / f"{name}.stats")
             return retval
 
         return wrapper
