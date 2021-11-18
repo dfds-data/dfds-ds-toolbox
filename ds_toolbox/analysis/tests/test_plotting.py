6bd96ce9b902539dcca8e200d6b17c00a9f8db5c
Kasper Primdal Lauritzen <klaur@dfds.com>
2021-11-18 12:29:36 +0100

    Add test fixture.
    Add explaination for failing asserts

--- ds_toolbox/analysis/tests/test_plotting.py
+++ ds_toolbox/analysis/tests/test_plotting.py
@@ -1,16 +1,21 @@
+import pytest
 from matplotlib.figure import Figure
 from sklearn import datasets
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 
 
-def test_prediction_histogram():
+@pytest.fixture(scope="function")
+def classification_dataset():
+    # Make a synthetic dataset
+    return datasets.make_classification(random_state=0)
+
+
+def test_prediction_histogram(classification_dataset):
     from ds_toolbox.analysis.plotting import plot_classification_proba_histogram
 
-    # Make a synthetic dataset
-    data = datasets.make_classification()
-    X, y = data
-    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    X, y = classification_dataset
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
     clf = LogisticRegression()
     # Train a simple model
     clf.fit(X_train, y_train)
@@ -18,17 +23,20 @@ def test_prediction_histogram():
     # Plot this example dataset
     fig = plot_classification_proba_histogram(y_true=y_test, y_pred=preds)
     # Check that the figure is a matplotlib Figure
-    assert isinstance(fig, Figure)
+    assert isinstance(
+        fig, Figure
+    ), "plot_classification_proba_histogram did not return a matplotlib Figure"
 
 
-def test_lift_curve():
+def test_lift_curve(classification_dataset):
     from ds_toolbox.analysis.plotting import plot_lift_curve
 
-    X, y = datasets.make_classification(random_state=0)
+    X, y = classification_dataset
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    # Train a simple model
     clf = LogisticRegression()
     clf.fit(X_train, y_train)
-
     y_pred = clf.predict_proba(X_test)[:, 1]  # Select probabilities for class 1
 
     fig = plot_lift_curve(
@@ -38,4 +46,4 @@ def test_lift_curve():
     )
 
     # Check that the figure is a matplotlib Figure
-    assert isinstance(fig, Figure)
+    assert isinstance(fig, Figure), "plot_lift_curve did not return a matplotlib Figure"
