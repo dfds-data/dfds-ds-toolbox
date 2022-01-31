6d9cac61c64a2e15d6ad2528830fea6e6e802b78
Kasper Primdal Lauritzen <klaur@dfds.com>
2022-01-31 09:41:54 +0100

    Fix pypi classifiers

--- setup.py
+++ setup.py
@@ -14,8 +14,9 @@ setup(
     install_requires=["scikit-learn", "pandas", "numpy", "statsmodels", "rich", "matplotlib"],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "Programming Language :: Python 3.7",
-        "Programming Language :: Python 3.8",
-        "Programming Language :: Python 3.9",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
 )
